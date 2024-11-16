16895dd0be45b913bbb5c131012e09fc42e195c1
BernardasJuzumas <bernardas@juzumas.com>
2024-11-16 09:20:12 +0200

    creating deployment , testing and observability infrastructure to test the full solution

--- deployments/docker-compose/python-loader/loader.py
+++ deployments/docker-compose/python-loader/loader.py
@@ -5,15 +5,19 @@ import string
 import os
 from influxdb_client import InfluxDBClient, Point
 
+# Settings
+ping_interval = float(os.getenv('PING__INTERVAL', '0.1'))
+
+
 # InfluxDB client
 influx_url = os.getenv('INFLUXDB_URL', 'http://localhost:8086')
 influx_token = os.getenv('INFLUXDB_TOKEN', 'my-influxdb-token')
 influx_org = os.getenv('INFLUXDB_ORG', 'my-org')
 influx_bucket = os.getenv('INFLUXDB_BUCKET', 'my-bucket')
-ping_interval = float(os.getenv('PING__INTERVAL', '0.1'))
 
 client = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
 write_api = client.write_api()
+
 
 def generate_random_string(length=10):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
